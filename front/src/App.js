c879af5a2c21eb655f73484ba1eb9372fe5351fd
alfredepopi <alfred.epopi@gmail.com>
2022-07-08 23:39:07 +0200

    changement de port react

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Router, Routes, Switch, Link } from 'react-router-dom';
 import { Color } from './pages/Color';
-import { Info } from './pages/Edit';
 import { Creneaux } from './pages/Creneaux';
-import { Navigation } from './pages/Navigation';
+
 
 import './App.css';
 
